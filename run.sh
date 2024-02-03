a931a74bb8f536e97600b6bb3e6eec494c592ef8
Paulo Jorge PM <paulo.jorge.pm@gmail.com>
2024-02-03 17:43:05 +0000

    Backup

---
+++ run.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+set -xe
+
+THIS=$(dirname "$0")
+
+pushd ${THIS}
+  source ./models.sh
+
+  pip install --user $(get_python_wheel_url "$1")
+  pip install --user -r <(grep -v stt requirements.txt)
+
+  pulseaudio &
+
+  python mic_vad_streaming.py \
+	  --model $HOME/STT/models/model.tflite \
+	  --scorer $HOME/STT/models/huge-vocab.scorer \
+	  --file $HOME/STT/audio/2830-3980-0043.wav
+popd
